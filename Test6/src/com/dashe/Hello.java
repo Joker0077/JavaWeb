80e3bef48f4c5eda52289cdc2c4ab6ac65866839
JJoker0077 <1197779079@qq.com>
2017-06-29 23:57:44 +0800

    图书馆管理系统

---
+++ Test6/src/com/dashe/Hello.java
@@ -0,0 +1,43 @@
+package com.dashe;
+
+public class Hello {
+	private String  name;
+	private int age;
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Hello [name=" + name + ", age=" + age + "]";
+	}
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+
+}
